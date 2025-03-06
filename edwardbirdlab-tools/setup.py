f16cfe4005922db3f51aeefccd942b4e4af091a8
Edward Bird <edwardbirdlab@gmail.com>
2025-03-06 12:04:38 -0600

    Fixing setup
    Removing auto find packages

--- edwardbirdlab-tools/setup.py
+++ edwardbirdlab-tools/setup.py
@@ -1,22 +1,16 @@
-from setuptools import setup, find_packages
+from distutils.core import setup
 
 setup(
-    name='ebird_tools',               # Name of your tool
-    version='0.2',                # Version of your tool
-    description='A collection of tools that I have made and use for several unrelated projects',  # Brief description
-    author='Edward Bird',                 # Replace with your name
-    author_email='edwardbirdlab@gmail.com',  # Your email
-    url='https://github.com/edwardbirdlab/edwardbirdlab-tools',  # Project URL
-    packages=find_packages(),     # Automatically find and include packages
+    name='Edwardbirdlab-tools',
+    version='1.0',
+    description='A collection of tools that I have made and use for several unrelated projects',
+    author='Edward Bird',
+    author_email='edwardbirdlab@gmail.com',
+    url='https://github.com/edwardbirdlab/edwardbirdlab-tools',
+    packages=['shutil', 'argparse', 'pandas','tqdm'],
     entry_points={                # Create a CLI command
         'console_scripts': [
             'ebirdtools = ebird_tools.main:main',  # 'my-tool' is the command; main function in main.py
         ],
-    },
-    install_requires={  # Optional: List of dependencies
-        'shutil',
-        'argparse',
-        'pandas',
-        'tqdm'
     },
 )
