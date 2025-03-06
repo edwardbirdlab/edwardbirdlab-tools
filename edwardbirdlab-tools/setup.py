800662a59299cf47212e7a8cec8a536a8004b91e
Edward Bird <edwardbirdlab@gmail.com>
2025-03-06 11:52:50 -0600

    editing setup

--- edwardbirdlab-tools/setup.py
+++ edwardbirdlab-tools/setup.py
@@ -2,15 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name='ebird_tools',               # Name of your tool
-    version='0.1',                # Version of your tool
-    description='A tool for parsing and cleaning up nextflow pipelines',  # Brief description
+    version='0.2',                # Version of your tool
+    description='A collection of tools that I have made and use for several unrelated projects',  # Brief description
     author='Edward Bird',                 # Replace with your name
     author_email='edwardbirdlab@gmail.com',  # Your email
-    url='https://github.com/edwardbirdlab/nextflow-parse',  # Project URL
-    packages=find_packages(),     # Automatically find and include your packages
+    url='https://github.com/edwardbirdlab/edwardbirdlab-tools',  # Project URL
+    packages=find_packages(),     # Automatically find and include packages
     entry_points={                # Create a CLI command
         'console_scripts': [
-            'nf-parse-tools = ebird_tools.main:main',  # 'my-tool' is the command; main function in main.py
+            'ebirdtools = ebird_tools.main:main',  # 'my-tool' is the command; main function in main.py
         ],
     },
     install_requires={  # Optional: List of dependencies
